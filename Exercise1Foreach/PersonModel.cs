37b5a74e53705e1b70d5a5d64e779109ea921198
Joan Barcelo Fiol <joanbarcelo1@paucasesnovescifp.cat>
2020-10-21 18:20:23 +0200

    Created a project that shows 2 lists in the console using foreach.
    Created a project with Visual Studio that shows 2 lists in the console using foreach.
    The first list is an array of Strings, and the second is a list of PersonModel objects.
    PersonModel contains 2 attributes “FirstName” and “LastName”.

---
+++ Exercise1Foreach/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercise1Foreach
+{
+    internal class PersonModel
+    {
+        public PersonModel(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ Exercise1Foreach/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise1Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> nameList = new List<String> {"Jhon", "Mary", "Sue", "Greg", "Yolanda", "Jose", "Bill" };
+
+            foreach (String i in nameList)
+            {
+                Console.WriteLine("Hello "+i);
+            }
+
+            Console.WriteLine();
+
+            List<PersonModel> personList = new List<PersonModel> { 
+                new PersonModel("Tim","Corey"), 
+                new PersonModel("Bill", "McCoy"),
+                new PersonModel("Mary","Jones"),
+                new PersonModel("Sue","Smith")
+            };
+
+            foreach (PersonModel i in personList)
+            {
+                Console.WriteLine("Hello " + i.FirstName + " " + i.LastName);
+            }
+        }
+    }
+}
